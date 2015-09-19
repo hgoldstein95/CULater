76ae0810a1c13895092adbea3c96933e471c0721
Harrison Goldstein <hgoldstein95@gmail.com>
2015-09-19 01:20:48 -0400

    Moved modal trigger and changed users to users_count

--- client/js/home.js
+++ client/js/home.js
@@ -19,11 +19,7 @@ Template.home.helpers({
      time: '10:00 AM - 5:00PM',
      place: 'Duffield Hall',
      description: 'Come one, come all! Play Smash Bros.HULK SMASH! HULK SMASH! HULK SMASH!Lorem ipsum dolor sit amet, vocibus lucilius comprehensam duo cu, his modo enim graeci an. Nobis soleat mel ut. Cu quod veritus detracto eam, ea inimicus expetendis neglegentur has. Ne oratio principes mei, cu tollit reprimique usu.',
-     users: [
-       { user: "Helena" },
-       { user: "Crafty Beaver" },
-       { user: "Nom noms" }
-     ]
+     users_count: 3
    },
 	mapOptions: function() {
 		return {center: central_campus, zoom: 15};
