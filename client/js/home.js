662e5939ff22911ca988e099548e413774a5176c
Harrison Goldstein <hgoldstein95@gmail.com>
2015-09-19 00:54:23 -0400

    Home templating js

--- client/js/home.js
+++ client/js/home.js
@@ -1,11 +1,17 @@
 Template.home.helpers({
- tasks: [
-   { text: "This is task 1" },
-   { text: "This is task 2" },
-   { text: "This is task 3" }
- ],
- users: [
-   { user: "Helena" },
-   { user: "Crafty Beaver" },
-   { user: "Nom noms" }]
+   tasks: [
+     { text: "This is task 1" },
+     { text: "This is task 2" },
+     { text: "This is task 3" }
+   ],
+   event_info: {
+     time: '10:00 AM - 5:00PM',
+     place: 'Duffield Hall',
+     description: 'Come one, come all! Play Smash Bros.HULK SMASH! HULK SMASH! HULK SMASH!Lorem ipsum dolor sit amet, vocibus lucilius comprehensam duo cu, his modo enim graeci an. Nobis soleat mel ut. Cu quod veritus detracto eam, ea inimicus expetendis neglegentur has. Ne oratio principes mei, cu tollit reprimique usu.',
+     users: [
+       { user: "Helena" },
+       { user: "Crafty Beaver" },
+       { user: "Nom noms" }
+     ]
+   }
 });
