6a6019b284055c2f16765a7fc21c0893955607f2
Chris Colen <cdcolen@att.net>
2015-09-19 07:50:01 -0400

    fixed date comparisons and hours until event

--- client/js/eventForm.js
+++ client/js/eventForm.js
@@ -1,20 +1,86 @@
 Template.eventForm.events({
-  'submit form': function(evt) {
+  'submit form.add-event': function(evt) {
   	evt.preventDefault();
   	var name = evt.target.eventName.value;
-  	var description = evt.target.eventDescription.value;
+  	if(evt.target.eventDescription){
+  		var description = evt.target.eventDescription.value;
+  	}
   	var location = evt.target.eventLocation.value;
-  	var time = evt.target.eventTime.value;
+  	var date = evt.target.eventDate.value;
+  	if(evt.target.eventStartTime){
+  		var startTime = evt.target.eventStartTime.value;
+  	}
+  	if(evt.target.eventEndTime){
+  		var endTime = evt.target.eventEndTime.value;
+  	}
   	var attendees = [];
+  	var eventDate = new Date(date.split("-")[0],date.split("-")[1],date.split("-")[2], startTime.split(":")[0], startTime.split(":")[1], 0);
+  	var endDate = new Date(date.split("-")[0],date.split("-")[1],date.split("-")[2], endTime.split(":")[0], endTime.split(":")[1], 0);
+  	var currentDate = new Date();
+  	if(eventDate < currentDate || endDate <= eventDate || !name || !location || !date){
+  		$("#error-messages").show();
+  		return;
+  	}
     Events.insert({
     	adminId: Meteor.userId(),
     	name: name,
     	description: description,
     	location: location,
-    	time: time,
+    	date: date,
+    	startTime: startTime,
+    	endTime: endTime,
     	attendees: attendees
     });
     $("#modal-close").click();
     $("form")[0].reset();
+  },
+
+  'submit form.edit-event': function(evt) {
+  	evt.preventDefault();
+  	var eventId = evt.target.eventId.value;
+  	var name = evt.target.eventName.value;
+  	var description = evt.target.eventDescription.value;
+  	var location = evt.target.eventLocation.value;
+  	var date = evt.target.eventDate.value;
+  	var startTime = evt.target.eventStartTime.value;
+  	var endTime = evt.target.eventEndTime.value;
+  	var attendees = [];
+    Events.update({
+            _id: eventId
+        }, {
+            $set: {
+                adminId: Meteor.userId(),
+		    	name: name,
+		    	description: description,
+		    	location: location,
+		    	date: date,
+		    	startTime: startTime,
+		    	endTime: endTime,
+		    	attendees: attendees
+            }
+        });
+    $("#modal-close").click();
+    $("form")[0].reset();
   }
 });
+
+
+Template.eventForm.helpers({
+	'today': function() {
+    	var t = new Date();
+		var dd = t.getDate();
+		var mm = t.getMonth()+1;
+		var yyyy = t.getFullYear();
+
+		if(dd<10) {
+		    dd='0'+dd
+		} 
+
+		if(mm<10) {
+		    mm='0'+mm
+		} 
+
+		t = yyyy + "-" + mm+'-'+dd;
+		return t;
+	}
+})
--- client/js/home.js
+++ client/js/home.js
@@ -42,6 +42,18 @@ Template.home.onCreated(function(){
 	});
 });
 
+function tConvert (time) {
+  // Check correct time format and split into components
+  time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+
+  if (time.length > 1) { // If time format correct
+    time = time.slice (1);  // Remove full string match value
+    time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+    time[0] = +time[0] % 12 || 12; // Adjust hours
+  }
+  return time.join (''); // return adjusted time or original string
+}
+
 Template.home.helpers({
 
 	'events': function() {
@@ -49,17 +61,89 @@ Template.home.helpers({
     },
     'myEvent': function(eventId) {
     	return Meteor.userId() == Events.findOne({_id: eventId}).adminId;
+    },
+    'getDate': function(date) {
+    	date = date.split('-');
+    	var formatDate = new Date(date);
+    	return formatDate.getMonth()+1 + "/" + formatDate.getDate() + "/" + date[0]
+    },
+    'getTime': function(time) {
+    	return tConvert(time);
+    },
+    'getTimeToEvent': function(time) {
+    	var hours = parseInt(time.split(":")[0]);
+    	var minutes = parseInt(time.split(":")[1]);
+    	var totalMinutes = 60*hours + minutes
+    	var d = new Date();
+    	var tM = 60*d.getHours() + d.getMinutes();
+    	var diff = totalMinutes - tM
+  		if(diff<0){
+  			return 0;
+  		}
+  		if( Math.floor(diff/60) == 0){
+  			return "<1";
+  		}
+  		return Math.floor(diff/60);
+    },
+    'pluralize': function(time) {
+    	var hours = parseInt(time.split(":")[0]);
+    	var minutes = parseInt(time.split(":")[1]);
+    	var totalMinutes = 60*hours + minutes
+    	var d = new Date();
+    	var tM = 60*d.getHours() + d.getMinutes();
+    	var diff = totalMinutes - tM
+  		if( Math.floor(diff/60) == 1){
+  			return "hour";
+  		}
+    	return "hours";
+    },
+    'sameDay':function(date) {
+		var inputDate = new Date(date);
+		var todaysDate = new Date();
+		if(inputDate.getFullYear() == todaysDate.getFullYear() && inputDate.getMonth() == todaysDate.getMonth() && inputDate.getDate()+1 == todaysDate.getDate())
+		{
+    		return true;
+		}
+		return false;
+    },
+    'notAlreadyHappened':function(Event){
+    	date=Event.date;
+    	startTime=Event.startTime;
+    	endTime=Event.endTime;
+    	var eventDate = new Date(date.split("-")[0],date.split("-")[1],date.split("-")[2], startTime.split(":")[0], startTime.split(":")[1], 0);
+		var endDate = new Date(date.split("-")[0],date.split("-")[1],date.split("-")[2], endTime.split(":")[0], endTime.split(":")[1], 0);
+		var currentDate = new Date();
+		if(eventDate < currentDate || endDate <= eventDate){
+			Events.remove({
+				_id: Event._id
+			})
+			return false;
+		}
+		return true;
     }
 })
 
 Template.home.events({
 
+	'click #new-event': function(evt) {
+		$("#myModalLabel").html("New Event");
+		$("form.edit-event").attr("class","add-event");
+		$("form.add-event")[0].reset();
+    },
+
 	'click a#edit-event': function(evt) {
+		$("#myModalLabel").html("Edit Event");
 		var eventId = $(evt.target).data("eventid");
-		if(Meteor.userId() == Events.findOne({_id: eventId}).adminId) {
-	    	Events.remove({
-	    		_id: eventId
-	    	});
+		$("form.add-event").attr("class","edit-event");
+		$("#eventId").val(eventId);
+		var myEvent = Events.findOne({_id: eventId});
+		if(Meteor.userId() == myEvent.adminId) {
+	    	$("#eventName").val(myEvent.name);
+	    	$("#eventDescription").val(myEvent.description);
+	    	$("#eventLocation").val(myEvent.location);
+	    	$("#eventDate").val(myEvent.date);
+	    	$("#eventStartTime").val(myEvent.startTime);
+	    	$("#eventEndTime").val(myEvent.endTime);
     	}
     },
 
