9faa106c81cfe7845fd47ef195a10c07330325ea
Chris Colen <cdcolen@att.net>
2015-09-20 05:48:36 -0400

    commented out time filter

--- client/js/home.js
+++ client/js/home.js
@@ -399,7 +399,7 @@ Template.home.events({
 			eventsList = Events.find( { $and: [ {adminId: Meteor.userId()} ] },{sort: {"date": 1, "startTime": 1}});
 		}
 		if(date1 && date2){
-			$("#times-between").show();
+			//$("#times-between").show();
 			var newTime1 = time1;
 			var newTime2 = time2;
 			if(!time1 || !time2){
