ccb0c81aa7b0894f10c9082075272725aa2d6400
Chris Colen <cdcolen@att.net>
2015-09-20 05:36:38 -0400

    Fixed Calendar Issue

--- client/js/dialogModal.js
+++ client/js/dialogModal.js
@@ -1,6 +1,13 @@
 Template.dialogModal.events({
   'click .btn-primary': function(evt) {
     var eventId = $('#dialogModal').data('eventid');
+
+    Meteor.call('sendEmail',
+            'cdc99@cornell.edu',
+            'cdc99@cornell.edu',
+            'Hello from Meteor!',
+            'This is a test of Email.send.');
+
     if(Meteor.userId() == Events.findOne({_id: eventId}).adminId) {
       Events.remove({
         _id: eventId
--- server/server.js
+++ server/server.js
@@ -25,5 +25,18 @@ Meteor.startup(function(){
 });
 
 Meteor.methods({
-	eventsOnHooksInit : function(){}
+  sendEmail: function (to, from, subject, text) {
+    check([to, from, subject, text], [String]);
+
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+
+    Email.send({
+      to: to,
+      from: from,
+      subject: subject,
+      text: text
+    });
+  }
 });
